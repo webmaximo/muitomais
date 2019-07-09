b54f9d6633dbe4a2e0189990539514513fec8dd9
webmaximo <luciano.maximo.camargo@gmail.com>
2019-07-09 15:33:37 -0300

    APP Consumidor

---
+++ app/registro.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator)
+{
+    navigator.serviceWorker
+             .register('/pwabuilder-sw.js')
+             .then(function() {
+                 console.log('Service worker resgistered!');
+             });
+}
