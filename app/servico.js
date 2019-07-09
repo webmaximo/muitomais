f933bee239511476b62da07c568e9d8b7aad3c55
webmaximo <luciano.maximo.camargo@gmail.com>
2019-07-09 15:46:47 -0300

    Update servico.js

--- app/servico.js
+++ app/servico.js
@@ -1,10 +1,5 @@
 // This is the "Offline page" service worker
 
-const CACHE = "app-pwa";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
-
 //Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener('install', function(event) {
     var offlinePage = new Request('offline.html');
