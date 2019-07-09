f4e209e264643eb6f95049f65afd440368f0f167
webmaximo <luciano.maximo.camargo@gmail.com>
2019-07-09 15:43:30 -0300

    Update servico.js

--- app/servico.js
+++ app/servico.js
@@ -1,12 +1,17 @@
-//This is the "Offline page" service worker
+// This is the "Offline page" service worker
+
+const CACHE = "app-pwa";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
 
 //Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener('install', function(event) {
     var offlinePage = new Request('offline.html');
     event.waitUntil(
       fetch(offlinePage).then(function(response) {
-        return caches.open('pwabuilder-offline').then(function(cache) {
-          console.log('[PWA Builder] Cached offline page during Install'+ response.url);
+        return caches.open('app-pwa').then(function(cache) {
+          console.log('[PWA APP] Cached offline page during Install'+ response.url);
           return cache.put(offlinePage, response);
         });
     }));
@@ -17,8 +22,8 @@ self.addEventListener('install', function(event) {
   self.addEventListener('fetch', function(event) {
     event.respondWith(
       fetch(event.request).catch(function(error) {
-        console.error( '[PWA Builder] Network request Failed. Serving offline page ' + error );
-        return caches.open('pwabuilder-offline').then(function(cache) {
+        console.error( '[PWA APP] Network request Failed. Serving offline page ' + error );
+        return caches.open('app-pwa').then(function(cache) {
           return cache.match('offline.html');
         });
       }
@@ -27,8 +32,8 @@ self.addEventListener('install', function(event) {
   
   //This is a event that can be fired from your page to tell the SW to update the offline page
   self.addEventListener('refreshOffline', function(response) {
-    return caches.open('pwabuilder-offline').then(function(cache) {
-      console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
+    return caches.open('app-pwa').then(function(cache) {
+      console.log('[PWA APP] Offline page updated from refreshOffline event: '+ response.url);
       return cache.put(offlinePage, response);
     });
   });
