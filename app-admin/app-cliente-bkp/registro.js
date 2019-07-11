b1e35e8e7c267739a7dbc0283426257ff549fa1d
webmaximo <luciano.maximo.camargo@gmail.com>
2019-07-11 14:51:06 -0300

    Novos Testes

---
+++ app-admin/app-cliente-bkp/registro.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator)
+{
+    navigator.serviceWorker
+             .register('servico.js')
+             .then(function() {
+                 console.log('Service worker resgistered!');
+             });
+}
